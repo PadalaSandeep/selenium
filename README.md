d5905ef92747855b69a50b9c3c79c2b3511289e9
SANDEEP\padala <sandeep.dm@live.com>
2017-12-16 15:39:20 +0530

    no message

--- README.md
+++
@@ -1,2 +0,0 @@
-# selenium
-Selenium script based on Hybrid frame work
---
+++ src/variable/intl.java
@@ -0,0 +1,15 @@
+package variable;
+
+public class intl {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char a='S';
+		System.out.println("given character is " +a);
+		char n=67;
+		System.out.println("given character is " +n);
+		byte b=(byte) 245;
+		System.out.println("given number is " +b);
+	}
+
+}
